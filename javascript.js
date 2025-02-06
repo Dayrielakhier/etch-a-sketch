99ffbb172861770b9520d67e3768203ad104d054
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-02-06 16:44:40 +0800

    Add prompt for user to input number of grids
    When the button is clicked, a prompt will pop up.
    The number of grids will be generated according to the user's input.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,33 @@
 const div = document.querySelector("#container")
+const btn = document.querySelector("button")
 
-for (i = 0; i < 16; i++) {
-    const row = document.createElement("div")
-    row.classList.add("row")
-    for (j = 0;j < 16; j++) {
-        const grid = document.createElement("div")
-        grid.classList.add("grid")
-        grid.addEventListener("mouseenter", function () {
-            grid.style.background = "blue"
-        })
-        grid.addEventListener("mouseleave", function () {
-            grid.style.background = "initial"
-        })
-        row.appendChild(grid)
+function getGrids() {
+    return prompt("Enter a number of grids between 1-100")
+}
+
+btn.addEventListener("click", () => generateGrids(getGrids()))
+
+function generateGrids(grids) {
+    if (grids < 1 || grids > 100) {
+        console.log("Type a number between 1-100")
+    } else {
+        div.replaceChildren()
+        for (i = 0; i < grids; i++) {
+        const row = document.createElement("div")
+        row.classList.add("row")
+            for (j = 0;j < grids; j++) {
+            const grid = document.createElement("div")
+            grid.classList.add("grid")
+            grid.addEventListener("mouseenter", function () {
+                grid.style.background = "blue"
+            })
+            grid.addEventListener("mouseleave", function () {
+                grid.style.background = "initial"
+            })
+            row.appendChild(grid)
+            }
+        div.appendChild(row)
+        }
     }
-    div.appendChild(row)
 }
 
